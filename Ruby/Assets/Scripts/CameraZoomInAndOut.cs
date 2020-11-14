374a485ddc851ecea282681b2854b5bb50846bfa
LZY000126 <73522418+LZY000126@users.noreply.github.com>
2020-11-14 16:17:18 +0800

    add zoom in and out

---
+++ Ruby/Assets/Scripts/CameraZoomInAndOut.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraZoomInAndOut : MonoBehaviour
+{
+
+    public CinemachineFreeLook zoom;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            zoom.m_Lens.FieldOfView++;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+        {
+            zoom.m_Lens.FieldOfView--;
+        }
+    }
+}
