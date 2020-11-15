611d6d26aa594603d82a5a00cb6b3e860f7f543e
LZY000126 <73522418+LZY000126@users.noreply.github.com>
2020-11-15 16:14:17 +0800

    add gravity, movement, collision

---
+++ Ruby/Assets/Scripts/CameraZoomInAndOut.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraZoomInAndOut : MonoBehaviour
+{
+
+    public CinemachineFreeLook zoom;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            if (zoom.m_Lens.FieldOfView <= 50)
+            {
+                zoom.m_Lens.FieldOfView++;
+            }
+            else
+                zoom.m_Lens.FieldOfView = 50;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+        {
+            if (zoom.m_Lens.FieldOfView >= 20)
+                zoom.m_Lens.FieldOfView--;
+            else
+                zoom.m_Lens.FieldOfView = 20;
+        }
+    }
+}
