5754f2a9d0f4fb1d258957851285d47062600146
khenghaw <yewkh-wm18@student.tarc.edu.my>
2020-11-15 21:54:06 +0800

    addEnemy

---
+++ Ruby/Assets/Scripts/paladdinMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class paladdinMove : MonoBehaviour
+{
+    public Transform[] point;
+    private int finalPoint = 0;
+    private NavMeshAgent agent;
+    public int speed;
+    private double observeTime = 0.0;
+    private double observeFinishTime = 0.05;
+    public Animator anim;
+    public static bool noEnermy = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        GetComponent<NavMeshAgent>().speed = speed;
+        anim = GetComponent<Animator>();
+        agent.autoBraking = false;
+        anim.SetBool("isObserve", false);
+    }
+    void nextPoint()
+    {
+        if (noEnermy)
+        {
+            if (point.Length == 0)
+            {
+                return;
+            }
+            anim.SetBool("isObserve", false);
+            anim.SetBool("isWalk", true);
+            agent.destination = point[finalPoint].position;
+            finalPoint = (finalPoint + 1) % point.Length;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (noEnermy)
+        {
+            if (agent.remainingDistance < 0.5)
+            {
+                observeTime += (double)Time.deltaTime;
+                if (observeTime > observeFinishTime)
+                {
+                    observeTime = 0;
+                    nextPoint();
+                }
+
+            }
+        }
+    }
+}
---
+++ Ruby/Assets/Scripts/paladinDetectTarget.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class paladinDetectTarget : MonoBehaviour
+{
+    public Transform Enemy;
+    public Animator anim;
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 myCharacter = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Vector3 targetEnemy = new Vector3(Enemy.position.x, Enemy.position.y, Enemy.position.z);
+        if (Vector3.Distance(myCharacter, targetEnemy) <= 50 && Vector3.Distance(myCharacter, targetEnemy) > 41)
+        {
+            anim.SetBool("isWalk", false);
+            anim.SetBool("isChase", true);
+        }
+        else if (Vector3.Distance(myCharacter, targetEnemy) <= 40 && Vector3.Distance(myCharacter, targetEnemy) > 21)
+        {
+            anim.SetBool("isChase", false);
+            anim.SetBool("isAttack", true);
+        }
+        else if (Vector3.Distance(myCharacter, targetEnemy) <= 20 && Vector3.Distance(myCharacter, targetEnemy) > 11)
+        {
+            anim.SetBool("isAttack", false);
+            anim.SetBool("isSlay", true);
+        }
+        if (Vector3.Distance(myCharacter, targetEnemy) <= 50 && Vector3.Distance(myCharacter, targetEnemy) > 11)
+        {
+            transform.LookAt(targetEnemy);
+            transform.position += transform.forward * Time.deltaTime * speed;
+            paladdinMove.noEnermy = false;
+        }
+        else if (Vector3.Distance(myCharacter, targetEnemy) > 50)
+        {
+            paladdinMove.noEnermy = true;
+        }
+    }
+}
