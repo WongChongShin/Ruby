5acd2adc0016a2dfd002c5c4c9ebfba39ebc53fc
khenghaw <yewkh-wm18@student.tarc.edu.my>
2020-11-22 21:34:41 +0800

    update paladdin collision

--- Ruby/Assets/Scripts/paladdinMove.cs
+++ Ruby/Assets/Scripts/paladdinMove.cs
@@ -7,11 +7,10 @@ public class paladdinMove : MonoBehaviour
 {
     public Transform[] point;
     private int finalPoint = 0;
-    private NavMeshAgent agent;
+    public static NavMeshAgent agent;
     public int speed;
     public Animator anim;
     private int number = 0;
-    public static bool noEnermy = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,52 +23,50 @@ public class paladdinMove : MonoBehaviour
     }
     void nextPoint()
     {
-        if (noEnermy)
+        if (point.Length == 0)
         {
-            if (point.Length == 0)
-            {
-                return;
-            }
-            agent.isStopped = false;
-            anim.SetBool("isObserve", false);
-            anim.SetBool("isWalk", true);
-            agent.destination = point[finalPoint].position;
-            finalPoint = (finalPoint + 1) % point.Length;
+            return;
         }
+        agent.isStopped = false;
+        anim.SetBool("isObserve", false);
+        anim.SetBool("isWalk", true);
+        agent.destination = point[finalPoint].position;
+        finalPoint = (finalPoint + 1) % point.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (noEnermy)
-        {
-            if (agent.remainingDistance < 20)
-            {
-                agent.isStopped = true;
-                if (agent.remainingDistance < 5)
-                {
-                    anim.SetBool("isObserve", true);
-                    anim.SetBool("isWalk", false);
-                    if (number < 1)
-                    {
-                        StartCoroutine(wait());
-                    }
-                    number++;
-                }
-
-            }
-            else
-            {
-                anim.SetBool("isObserve", false);
-                anim.SetBool("isWalk", true);
-                agent.isStopped = false;
-                number = 0;
-            }
-        }
+        movement();
     }
     IEnumerator wait()
     {
         yield return new WaitForSeconds(5);
         nextPoint();
+    }
+    public void movement()
+    {
+        if (agent.remainingDistance < 20)
+        {
+            agent.isStopped = true;
+            if (agent.remainingDistance < 5)
+            {
+                anim.SetBool("isObserve", true);
+                anim.SetBool("isWalk", false);
+                if (number < 1)
+                {
+                    StartCoroutine(wait());
+                }
+                number++;
+            }
+
+        }
+        else
+        {
+            anim.SetBool("isObserve", false);
+            anim.SetBool("isWalk", true);
+            agent.isStopped = false;
+            number = 0;
+        }
     }
 }
--- Ruby/Assets/Scripts/paladinDetectTarget.cs
+++ Ruby/Assets/Scripts/paladinDetectTarget.cs
@@ -8,49 +8,65 @@ public class paladinDetectTarget : MonoBehaviour
     public Transform Enemy;
     public Animator anim;
     public int speed;
+    private paladdinMove movePaladdin;
+    private Vector3 myCharacter;
+    private Vector3 targetEnemy;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        movePaladdin = gameObject.GetComponent<paladdinMove>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 myCharacter = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        Vector3 targetEnemy = new Vector3(Enemy.position.x, Enemy.position.y, Enemy.position.z);
-        if (Vector3.Distance(myCharacter, targetEnemy) <= 70 && Vector3.Distance(myCharacter, targetEnemy) > 51)
+        detectionChecker();
+    }
+    void changeAnimation()
+    {
+        if (Vector3.Distance(myCharacter, targetEnemy) <= 70 && Vector3.Distance(myCharacter, targetEnemy) > 31)
         {
+
             anim.SetBool("isWalk", false);
             anim.SetBool("isChase", true);
+            anim.SetBool("isSlay", false);
+            anim.SetBool("isAttack", false);
         }
-        else if (Vector3.Distance(myCharacter, targetEnemy) <= 50 && Vector3.Distance(myCharacter, targetEnemy) > 31)
+        else if (Vector3.Distance(myCharacter, targetEnemy) <= 30 && Vector3.Distance(myCharacter, targetEnemy) > 17)
         {
+            anim.SetBool("isWalk", false);
             anim.SetBool("isChase", false);
+            anim.SetBool("isSlay", false);
             anim.SetBool("isAttack", true);
         }
-        else if (Vector3.Distance(myCharacter, targetEnemy) <= 30 && Vector3.Distance(myCharacter, targetEnemy) > 5)
+        else if (Vector3.Distance(myCharacter, targetEnemy) <= 16 && Vector3.Distance(myCharacter, targetEnemy) > 5)
         {
+            anim.SetBool("isChase", false);
+            anim.SetBool("isWalk", false);
             anim.SetBool("isAttack", false);
             anim.SetBool("isSlay", true);
         }
+    }
+    public void detectionChecker()
+    {
+        myCharacter = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        targetEnemy = new Vector3(Enemy.position.x, Enemy.position.y, Enemy.position.z);
         if (Vector3.Distance(myCharacter, targetEnemy) <= 70 && Vector3.Distance(myCharacter, targetEnemy) > 5)
         {
             transform.LookAt(targetEnemy);
             transform.position += transform.forward * Time.deltaTime * speed;
-            paladdinMove.noEnermy = false;
+            if (Vector3.Distance(myCharacter, targetEnemy) <= 70 && Vector3.Distance(myCharacter, targetEnemy) > 7)
+            {
+                paladdinMove.agent.isStopped = true;
+            }
+            changeAnimation();
+
         }
-        else if(Vector3.Distance(myCharacter, targetEnemy)<=5)
+        else if (Vector3.Distance(myCharacter, targetEnemy) > 70)
         {
-            paladdinMove.noEnermy = false;
+            paladdinMove.agent.isStopped = false;
+            movePaladdin.movement();
         }
-        else if (Vector3.Distance(myCharacter, targetEnemy) > 50)
-        {
-            paladdinMove.noEnermy = true;
-            anim.SetBool("isWalk", true);
-            anim.SetBool("isAttack", false);
-            anim.SetBool("isSlay", false);
-        }
-        print(paladdinMove.noEnermy);
     }
 }
