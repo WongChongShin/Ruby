ddf1de07287906f7f1ab2380b6bff9959171aede
khenghaw <yewkh-wm18@student.tarc.edu.my>
2020-11-23 01:24:30 +0800

    add spartan king script and warrior

---
+++ Ruby/Assets/Scripts/warriorDelegate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class warriorDelegate : MonoBehaviour
+{
+    public delegate void MyDelegate();
+    public static event MyDelegate enemyDelegate;
+    private warriorDetectEnemy detect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        detect = gameObject.GetComponent<warriorDetectEnemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        warriorDelegate.enemyDelegate = detect.detectionChecker;
+        enemyDelegate();
+    }
+}
