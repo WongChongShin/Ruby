d9acd857c8e0d768fc21925070b870033f1472c2
khenghaw <yewkh-wm18@student.tarc.edu.my>
2020-11-23 19:27:12 +0800

    Revert "Revert "add spartan king script and warrior""
    This reverts commit 25daa25e11b5d47955c8eed06d5a3579b3cf5708.

---
+++ Ruby/Assets/Scripts/spartanKingMove.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class spartanKingMove : MonoBehaviour
+{
+    public Transform[] point;
+    private int finalPoint = 0;
+    public static NavMeshAgent agent;
+    public int speed;
+    public Animator anim;
+    private int number = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        GetComponent<NavMeshAgent>().speed = speed;
+        anim = GetComponent<Animator>();
+        agent.autoBraking = false;
+        anim.SetBool("isWalking", true);
+        nextPoint();
+    }
+    void nextPoint()
+    {
+        if (point.Length == 0)
+        {
+            return;
+        }
+        agent.isStopped = false;
+        anim.SetBool("isWalking", true);
+        agent.destination = point[finalPoint].position;
+        finalPoint = (finalPoint + 1) % point.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(5);
+        nextPoint();
+    }
+    public void movement()
+    {
+        if (agent.remainingDistance < 20)
+        {
+            agent.isStopped = true;
+            if (agent.remainingDistance < 5)
+            {
+                anim.SetBool("isWalking", false);
+                if (number < 1)
+                {
+                    StartCoroutine(wait());
+                }
+                number++;
+            }
+
+        }
+        else
+        {
+            anim.SetBool("isWalking", true);
+            agent.isStopped = false;
+            number = 0;
+        }
+    }
+}
