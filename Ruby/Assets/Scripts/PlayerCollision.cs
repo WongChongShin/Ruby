827eec855f10b9e6887cbed7805a01b07ce93ec3
WongChongShin <73522463+WongChongShin@users.noreply.github.com>
2020-11-23 19:18:24 +0800

    Revert "doing box pick up"
    This reverts commit ee5365ea5fc19167cad3c9f1b4423a8d30a62f9f.

--- Ruby/Assets/Scripts/PlayerCollision.cs
+++ Ruby/Assets/Scripts/PlayerCollision.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     private bool rubyIsDead = false;
-    //private Transform carriedBox;
 
     public AudioClip rubyDieSound;   
     public AudioClip coinIsCollected;
