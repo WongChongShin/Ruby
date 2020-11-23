0df732f12e153cab3cd5793d300bf25481bb3845
WongChongShin <73522463+WongChongShin@users.noreply.github.com>
2020-11-23 18:58:42 +0800

    Revert "revert this"
    This reverts commit 39fee220e2e90e571a395a2d2575f0338d4e8e28, reversing
    changes made to ee5365ea5fc19167cad3c9f1b4423a8d30a62f9f.

--- Ruby/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
+++ Ruby/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
@@ -28,9 +28,9 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		Vector3 m_CapsuleCenter;
 		CapsuleCollider m_Capsule;
 		bool m_Crouching;
-        private float timer = 0.0f;
 
-        void Start()
+
+		void Start()
 		{
 			m_Animator = GetComponent<Animator>();
 			m_Rigidbody = GetComponent<Rigidbody>();
@@ -41,8 +41,9 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
 			m_OrigGroundCheckDistance = m_GroundCheckDistance;
 		}
-               
-		public void Move(Vector3 move, bool crouch, bool jump, bool dodge)
+
+
+		public void Move(Vector3 move, bool crouch, bool jump)
 		{
 
 			// convert the world relative moveInput vector into a local-relative
@@ -60,7 +61,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			// control and velocity handling is different when grounded and airborne:
 			if (m_IsGrounded)
 			{
-				HandleGroundedMovement(crouch, jump, dodge);
+				HandleGroundedMovement(crouch, jump);
 			}
 			else
 			{
@@ -137,10 +138,10 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			{
 				m_Animator.SetFloat("JumpLeg", jumpLeg);
 			}
-            
-            // the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
-            // which affects the movement speed because of the root motion.
-            if (m_IsGrounded && move.magnitude > 0)
+
+			// the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
+			// which affects the movement speed because of the root motion.
+			if (m_IsGrounded && move.magnitude > 0)
 			{
 				m_Animator.speed = m_AnimSpeedMultiplier;
 			}
@@ -162,38 +163,18 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		}
 
 
-        void HandleGroundedMovement(bool crouch, bool jump, bool dodge)
-        {
-            // check whether conditions are right to allow a jump:
-            if (jump && !crouch && m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded"))
-            {
-                // jump!
-                m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, m_JumpPower, m_Rigidbody.velocity.z);
-                m_IsGrounded = false;
-                m_Animator.applyRootMotion = false;
-                m_GroundCheckDistance = 0.1f;
-            }
-
-            if (dodge && m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded"))
-            {
-                m_Animator.SetBool("IsDodge", true);
-                timer += Time.deltaTime;
-                print("timer: " + timer);
-
-                if (timer > 0.42)
-                {
-                    dodge = false;
-                    timer = 0.0f;
-                    
-                }
-
-                if (dodge == false)
-                    m_Animator.SetBool("IsDodge", false);
-
-                print("dodge: " + dodge);
-            }
-            
-        }
+		void HandleGroundedMovement(bool crouch, bool jump)
+		{
+			// check whether conditions are right to allow a jump:
+			if (jump && !crouch && m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded"))
+			{
+				// jump!
+				m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, m_JumpPower, m_Rigidbody.velocity.z);
+				m_IsGrounded = false;
+				m_Animator.applyRootMotion = false;
+				m_GroundCheckDistance = 0.1f;
+			}
+		}
 
 		void ApplyExtraTurnRotation()
 		{
--- Ruby/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Ruby/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -12,13 +12,10 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private Vector3 m_CamForward;             // The current forward direction of the camera
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
-        public Animator anim;
-        public GameObject player;
+
         
         private void Start()
         {
-            anim = GetComponent<Animator>();
-
             // get the transform of the main camera
             if (Camera.main != null)
             {
@@ -52,7 +49,6 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
             float v = CrossPlatformInputManager.GetAxis("Vertical");
             bool crouch = Input.GetKey(KeyCode.LeftControl);
-            bool dodge = Input.GetKey(KeyCode.LeftShift);
 
             // calculate move direction to pass to character
             if (m_Cam != null)
@@ -67,14 +63,13 @@ namespace UnityStandardAssets.Characters.ThirdPerson
                 m_Move = v*Vector3.forward + h*Vector3.right;
             }
 #if !MOBILE_INPUT
-            // walk speed multiplier
-            //if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+			// walk speed multiplier
+	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
 #endif
 
             // pass all parameters to the character control script
-            m_Character.Move(m_Move, crouch, m_Jump, dodge);
+            m_Character.Move(m_Move, crouch, m_Jump);
             m_Jump = false;
-            dodge = false;
         }
     }
 }
