a01f588deeb29282e735cfc57234c2aee22f4522
khenghaw <yewkh-wm18@student.tarc.edu.my>
2020-11-25 22:59:15 +0800

    Revert "backgroundMusic finish"
    This reverts commit 984a5107133bc2aebd2018de9e75c024ac81bfb6.

---
+++ Ruby/Assets/Scripts/warriorMove.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class warriorMove : MonoBehaviour
+{
+    public Transform[] point;
+    private int finalPoint = 0;
+    public NavMeshAgent agent;
+    public int speed;
+    public Animator anim;
+    private int number = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        GetComponent<NavMeshAgent>().speed = speed;
+        anim = GetComponent<Animator>();
+        agent.autoBraking = false;
+        nextPoint();
+    }
+    void nextPoint()
+    {
+        if (point.Length == 0)
+        {
+            return;
+        }
+        agent.isStopped = false;
+        agent.destination = point[finalPoint].position;
+        finalPoint = (finalPoint + 1) % point.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(5);
+        nextPoint();
+    }
+    public void movement()
+    {
+        if (agent.remainingDistance < 20)
+        {
+            agent.isStopped = true;
+            if (agent.remainingDistance < 5)
+            {
+                anim.SetBool("isVictory", true);
+                if (number < 1)
+                {
+                    StartCoroutine(wait());
+                }
+                number++;
+            }
+
+        }
+        else
+        {
+            anim.SetBool("isVictory", false);
+            agent.isStopped = false;
+            number = 0;
+        }
+    }
+}
