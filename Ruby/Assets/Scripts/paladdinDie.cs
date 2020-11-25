6adef6e16a007a3c9a9392fa14c1e6424b8738d4
khenghaw <yewkh-wm18@student.tarc.edu.my>
2020-11-25 23:00:00 +0800

    Revert "Revert "backgroundMusic finish""
    This reverts commit a01f588deeb29282e735cfc57234c2aee22f4522.

---
+++ Ruby/Assets/Scripts/paladdinDie.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class paladdinDie : MonoBehaviour
+{
+    private paladdinMove movePaladdin;
+    public Animator anim;
+    public AudioClip enemyDieSound;
+    public static bool die=true;
+    private NavMeshAgent agent;
+    AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePaladdin = gameObject.GetComponent<paladdinMove>();
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (die == true)
+        {
+            movePaladdin.agent.isStopped = true;
+            anim.SetBool("isWalk", false);
+            //anim.SetBool("isChase", false);
+            //anim.SetBool("isSlay", false);
+            //anim.SetBool("isAttack", false);
+            //anim.SetBool("isObserve", false);
+            anim.SetBool("isDie", true);
+        }
+    }
+    void OnTriggerEnter(Collider collisionInfo)
+    {
+        if (collisionInfo.gameObject.tag == "box")
+        {
+            
+            enemyDie();
+        }
+    }
+    public void enemyDie()
+    {
+        audio.clip = enemyDieSound;
+            audio.Play();
+            Destroy(gameObject, 3);
+    }
+}
