a5d44cd268210a6b5cf24772e2478aefcb194c9e
LZY000126 <73522418+LZY000126@users.noreply.github.com>
2020-11-26 13:29:56 +0800

    Revert "Revert "Revert "Revert "backgroundMusic finish""""
    This reverts commit 6d8e86ff473c1943e680ddf9e89614f67da49aea.

--- Ruby/Assets/Scripts/delegatePaladdin.cs
+++ Ruby/Assets/Scripts/delegatePaladdin.cs
@@ -7,10 +7,12 @@ public class delegatePaladdin : MonoBehaviour
     public delegate void MyDelegate();
     public static event MyDelegate enemyDelegate;
     private paladinDetectTarget detect;
+    private paladdinDie paladdin_die;
     // Start is called before the first frame update
     void Start()
     {
         detect = gameObject.GetComponent<paladinDetectTarget>();
+        paladdin_die = gameObject.GetComponent<paladdinDie>();
     }
 
     // Update is called once per frame
