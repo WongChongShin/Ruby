65f1fd1380d0e7cd7d2aa82738b171f9999d01d8
khenghaw <yewkh-wm18@student.tarc.edu.my>
2020-11-27 22:24:50 +0800

    spartan king script

--- Ruby/Assets/Scripts/spartanKingDetectTarget.cs
+++ Ruby/Assets/Scripts/spartanKingDetectTarget.cs
@@ -12,6 +12,10 @@ public class spartanKingDetectTarget : MonoBehaviour
     private Vector3 myCharacter;
     private Vector3 targetEnemy;
     private NavMeshAgent agent;
+    public GameObject electricalVolt;
+    private bool particleOn = false;
+    private bool startChase = false;
+    public Transform[] door;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,43 +27,87 @@ public class spartanKingDetectTarget : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        getDoorPosition();
         detectionChecker();
     }
     void changeAnimation()
     {
-        if (Vector3.Distance(myCharacter, targetEnemy) <= 100 && Vector3.Distance(myCharacter, targetEnemy) > 50)
-        {
-
-            anim.SetBool("isWalking", false);
-            anim.SetBool("isCharge", true);
-            anim.SetBool("isAttack", false);
-        }
-        else if (Vector3.Distance(myCharacter, targetEnemy) <= 49 && Vector3.Distance(myCharacter, targetEnemy) > 5)
+        
+        if (Vector3.Distance(myCharacter, targetEnemy) <= 49 && Vector3.Distance(myCharacter, targetEnemy) > 0)
         {
             anim.SetBool("isWalking", false);
             anim.SetBool("isCharge", false);
             anim.SetBool("isAttack", true);
+        }
+        else
+        {
+            if (!electricalVolt.GetComponent<ParticleSystem>().isPlaying)
+            {
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isCharge", true);
+                anim.SetBool("isAttack", false);
+            }
+            else
+            {
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isCharge", false);
+                anim.SetBool("isAttack", false);
+            }
         }
     }
     public void detectionChecker()
     {
         myCharacter = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         targetEnemy = new Vector3(Enemy.position.x, Enemy.position.y, Enemy.position.z);
-        if (Vector3.Distance(myCharacter, targetEnemy) <= 100 && Vector3.Distance(myCharacter, targetEnemy) > 5)
+        if (startChase == true)
         {
-            transform.LookAt(targetEnemy);
-            transform.position += transform.forward * Time.deltaTime * speed;
-            if (Vector3.Distance(myCharacter, targetEnemy) <= 100 && Vector3.Distance(myCharacter, targetEnemy) > 7)
+            if (Vector3.Distance(myCharacter, targetEnemy) > 15)
             {
-                moveSpartanKing.agent.isStopped = true;
+                if (!electricalVolt.GetComponent<ParticleSystem>().isPlaying)
+                {
+                    transform.LookAt(targetEnemy);
+                    transform.position += transform.forward * Time.deltaTime * speed;
+                }
             }
             changeAnimation();
+            if (Vector3.Distance(myCharacter, targetEnemy) >49)
+            {
+                if (particleOn == false)
+                {
+                    particleOn = true;
+                    StartCoroutine(wait1());
+                }
+            }
 
         }
-        else if (Vector3.Distance(myCharacter, targetEnemy) > 70)
+    }
+    void electronicCreate()
+    {
+        if (electricalVolt.GetComponent<ParticleSystem>().isPlaying)
         {
-            moveSpartanKing.agent.isStopped = false;
-            moveSpartanKing.movement();
+            electricalVolt.GetComponent<ParticleSystem>().Stop();
+            particleOn = false;
+        }
+        else
+        {
+            electricalVolt.GetComponent<ParticleSystem>().Play();
+            particleOn = false;
+        }
+    }
+    IEnumerator wait1()
+    {
+        yield return new WaitForSeconds(2);
+        electronicCreate();
+    }
+    void getDoorPosition()
+    {
+        for (int i = 0; i < door.Length; i++)
+        {
+            if (door[i].position.y > 70)
+            {
+                startChase = true;
+
+            }
         }
     }
 }
