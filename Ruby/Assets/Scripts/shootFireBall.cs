3500430c85d3a6121d6c6d12a12c011bc3b805c8
khenghaw <yewkh-wm18@student.tarc.edu.my>
2020-11-28 19:35:05 +0800

    cannon(Finish)

--- Ruby/Assets/Scripts/shootFireBall.cs
+++ Ruby/Assets/Scripts/shootFireBall.cs
@@ -8,31 +8,84 @@ public class shootFireBall : MonoBehaviour
     public GameObject fireball;
     public Transform player;
     public GameObject aimUI;
+    public GameObject fps;
+    private bool aim=false;
+    private bool checkAim = false;
+    public float throwForce;
     // Start is called before the first frame update
     void Start()
     {
-        //aimUI = GetComponent<RawImage>();
+        fireball.GetComponent<ParticleSystem>().Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        fireball.GetComponent<ParticleSystem>().Play();
+        if (aim ==true)
+        {
+            controlCannon();
+        }
     }
     void OnTriggerEnter(Collider collisionInfo)
     {
         if(collisionInfo.gameObject.tag == "Player")
         {
-            aimUI.SetActive(true);
-            if (Input.GetButtonDown("equip cannon"))
-            {
-                print("here");
-                controlCannon();
-            }
+            aim = true;
+        }
+        else
+        {
+            aim = false;
         }
     }
     void controlCannon()
     {
-        aimUI.SetActive(false);
+        if (Input.GetButtonDown("equip cannon"))
+        {
+            if (checkAim == false)
+            {
+                
+                (player.GetComponent<Animator>()).enabled = false;
+                //aimUI.SetActive(true);
+                checkAim = true;
+            }
+            else
+            {
+                (player.GetComponent<Animator>()).enabled = true;
+                //aimUI.SetActive(false);
+                checkAim = false;
+            }
+        }
+        if (checkAim == true)
+        {
+            mouseMovement();
+            shoot();
+        }
+        else
+        {
+            fps.SetActive(false);
+        }
+    }
+    void mouseMovement()
+    {
+        fps.SetActive(true);
+        (transform.GetComponent("FirstPersonController") as MonoBehaviour).enabled = true;
+
+    }
+    void shoot()
+    {
+        if (Input.GetButtonDown("shoot"))
+        {
+            Vector3 createPosition = new Vector3(transform.position.x, transform.position.y + 10, transform.position.z);
+            GameObject temp = Instantiate(fireball, createPosition, transform.rotation);
+
+            if (temp.GetComponent<Rigidbody>() == null)
+            {
+                Debug.Log("no Rigidbody found!");
+                temp.AddComponent<Rigidbody>();
+                temp.AddComponent<destroyFireBall>();
+            }
+            Rigidbody rb = temp.GetComponent<Rigidbody>();
+            rb.velocity = fireball.transform.TransformDirection(new Vector3(0, 0, throwForce));
+        }
     }
 }
