c3d6bd43519fff84b439cf7b703bb900b32f7012
khenghaw <yewkh-wm18@student.tarc.edu.my>
2020-12-01 17:22:11 +0800

    bossLevel

---
+++ Ruby/Assets/Scripts/spawnBall.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnBall : MonoBehaviour
+{
+    public GameObject ball;
+    public Transform[] door;
+    public int spawnTimeSecond = 0;
+    private bool startSpawn = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check for the box spawn or not
+        if (startSpawn == false)
+        {
+            //start to spawn the box
+            getDoorPosition();
+        }
+    }
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(spawnTimeSecond);
+        createBall();
+
+    }
+    void createBall()
+    {
+        Instantiate(ball, transform.position, transform.rotation);
+        startSpawn = false;//the box end spawning
+    }
+    void getDoorPosition()
+    {
+        for (int i = 0; i < door.Length; i++)
+        {
+            if (door[i].position.y > 70)
+            {
+                startSpawn = true;
+
+            }
+        }
+        if (startSpawn == true)
+        {
+            StartCoroutine(wait());
+        }
+    }
+}
