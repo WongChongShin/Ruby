03af4029ea21f2b825b3fd4d973c330424d4895c
WongChongShin <73522463+WongChongShin@users.noreply.github.com>
2020-12-01 23:13:23 +0800

    Win scene

---
+++ Ruby/Assets/Scripts/RubyWin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyWin : MonoBehaviour
+{
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isVictory", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+       
+        
+    }
+}
---
+++ Ruby/Assets/Scripts/RubyWinHint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RubyWinHint : MonoBehaviour
+{
+    private Text victoryHint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        victoryHint = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(wait());
+
+        IEnumerator wait()
+        {
+            yield return new WaitForSeconds(1);
+            victoryHint.text = "You Win!";
+        }       
+    }
+}
