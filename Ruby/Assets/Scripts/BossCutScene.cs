9b0aca0e9e12f119722b1d4bb7b9446798f97091
khenghaw <yewkh-wm18@student.tarc.edu.my>
2020-12-04 05:34:47 +0800

    script

--- Ruby/Assets/Scripts/BossCutScene.cs
+++ Ruby/Assets/Scripts/BossCutScene.cs
@@ -6,30 +6,45 @@ using UnityEngine.SceneManagement;
 public class BossCutScene : MonoBehaviour
 {
     private string levelToLoad;
-    public static bool changeScene = false;
+    private bool changeScene = false;
+    public Transform[] door;
+    private bool notReadDoor = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        getDoorPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (notReadDoor == false)
+        {
+            getDoorPosition();
+        }
         if (changeScene == true)
         {
-            
-
+            changeScene = false;
             levelToLoad = "BossCutScene";
             StartCoroutine(Wait());
-
-            IEnumerator Wait()
+        }
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(levelToLoad);
+    }
+    void getDoorPosition()
+    {
+        for (int i = 0; i < door.Length; i++)
+        {
+            if (door[i].position.y > 40)
             {
-                yield return new WaitForSeconds(1.0f);
-                SceneManager.LoadScene(levelToLoad);
+                changeScene = true;
+                notReadDoor = true;
+
             }
-            changeScene = false;
         }
     }
 }
