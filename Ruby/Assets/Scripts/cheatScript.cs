3c63f34510d09f3691048ce928a2b7d931fd24ca
khenghaw <yewkh-wm18@student.tarc.edu.my>
2020-12-04 00:16:46 +0800

    cheat health press p

---
+++ Ruby/Assets/Scripts/cheatScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cheatScript : MonoBehaviour
+{
+    private bool cheatOn = false;
+    private bool checkCheat = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthCheat();
+        if (cheatOn == true)
+        {
+            if (healthPoint.health < 3)
+            {
+                healthPoint.health = 3;
+            }
+        }
+    }
+
+    void healthCheat()
+    {
+        if (Input.GetButtonDown("cheat health"))
+        {
+            if (cheatOn == false)
+            {
+                cheatOn = true;
+            }
+            else if (cheatOn == true)
+            {
+                cheatOn = false;
+            }
+        }
+    }
+}
